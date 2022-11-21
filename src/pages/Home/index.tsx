2ab5dc63fdae82141d12cd169ee9fe37f609475d
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2022-11-20 20:36:45 -0400

    feat: Home page - DONE

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -19,10 +19,24 @@ export function Home() {
             type="text"
             id="task"
             placeholder="Dê um nome para o seu projeto"
+            list="task-suggestions"
           />
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmmount">durante:</label>
-          <MinutesAmount type="number" id="minutesAmmount" placeholder="00" />
+          <MinutesAmount
+            type="number"
+            id="minutesAmmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
 
           <span>minutos.</span>
         </FormContainer>

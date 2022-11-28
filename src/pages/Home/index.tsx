94fafde3b4da66acd55841ba545c0510bca81732
Tafarel-Brayan <tafarelbrayan@hotmail.com>
2022-11-27 23:12:59 -0400

    feat: Reduzindo countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -12,7 +12,8 @@ import {
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
 import { useForm } from "react-hook-form";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { differenceInSeconds } from "date-fns";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
@@ -28,6 +29,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -43,19 +45,31 @@ export function Home() {
     },
   });
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+
   const handleCreateNewCycle = (data: NewCycleFormData) => {
     const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     };
 
-    setCycles((state) => [...state, newCycle]); // (state) é o meu statdo atual, ou seja, tudo o que tiver em cycles
+    setCycles((state) => [...state, newCycle]); // (state) é o meu estado atual, ou seja, tudo o que tiver em cycles
     setActiveCycleId(newCycle.id);
     reset();
   };
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
   const minutesAmount = Math.floor(currentSeconds / 60);
